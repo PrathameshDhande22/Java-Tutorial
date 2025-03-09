96dc4dd040bd311cb4419ad857be1ade2aade909
Prathameshdhande22 <prathameshdhande534@gmail.com>
2025-03-09 17:35:18 +0530

    Navbar layout type added with support for QuartzComponent

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -8,41 +8,41 @@ import { Theme } from "./util/theme"
 export type Analytics =
   | null
   | {
-      provider: "plausible"
-      host?: string
-    }
+    provider: "plausible"
+    host?: string
+  }
   | {
-      provider: "google"
-      tagId: string
-    }
+    provider: "google"
+    tagId: string
+  }
   | {
-      provider: "umami"
-      websiteId: string
-      host?: string
-    }
+    provider: "umami"
+    websiteId: string
+    host?: string
+  }
   | {
-      provider: "goatcounter"
-      websiteId: string
-      host?: string
-      scriptSrc?: string
-    }
+    provider: "goatcounter"
+    websiteId: string
+    host?: string
+    scriptSrc?: string
+  }
   | {
-      provider: "posthog"
-      apiKey: string
-      host?: string
-    }
+    provider: "posthog"
+    apiKey: string
+    host?: string
+  }
   | {
-      provider: "tinylytics"
-      siteId: string
-    }
+    provider: "tinylytics"
+    siteId: string
+  }
   | {
-      provider: "cabin"
-      host?: string
-    }
+    provider: "cabin"
+    host?: string
+  }
   | {
-      provider: "clarity"
-      projectId?: string
-    }
+    provider: "clarity"
+    projectId?: string
+  }
 
 export interface GlobalConfiguration {
   pageTitle: string
@@ -85,6 +85,7 @@ export interface QuartzConfig {
 export interface FullPageLayout {
   head: QuartzComponent
   header: QuartzComponent[]
+  navbar: QuartzComponent[]
   beforeBody: QuartzComponent[]
   pageBody: QuartzComponent
   afterBody: QuartzComponent[]
@@ -94,4 +95,4 @@ export interface FullPageLayout {
 }
 
 export type PageLayout = Pick<FullPageLayout, "beforeBody" | "left" | "right">
-export type SharedLayout = Pick<FullPageLayout, "head" | "header" | "footer" | "afterBody">
+export type SharedLayout = Pick<FullPageLayout, "head" | "navbar" | "header" | "footer" | "afterBody">
