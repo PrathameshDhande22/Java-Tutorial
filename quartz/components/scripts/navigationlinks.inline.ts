2b796191dc065e57f8a82784392accee96c81c22
Prathameshdhande22 <prathameshdhande534@gmail.com>
2025-03-11 00:24:04 +0530

    Clicking Menu button now activates Drawer Component instead of links absolute blocks

--- quartz/components/scripts/navigationlinks.inline.ts
+++ quartz/components/scripts/navigationlinks.inline.ts
@@ -1,5 +1,5 @@
-let links = document.getElementsByClassName("links")[0]
+let drawer = document.getElementsByClassName("drawer")[0]
 
 document.getElementsByClassName("menu-btn")[0].addEventListener("click", () => {
-    links.classList.toggle("active")
+    drawer.classList.toggle("active")
 })
