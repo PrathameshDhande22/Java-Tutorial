df4f6fdd37392f67c9c06cbd95bb3df785723ace
Prathameshdhande22 <prathameshdhande534@gmail.com>
2025-03-26 00:20:44 +0530

    Removed the Spaces from the Code

--- quartz/components/scripts/navigationlinks.inline.ts
+++ quartz/components/scripts/navigationlinks.inline.ts
@@ -4,7 +4,6 @@ let drawer = document.getElementsByClassName("drawer")[0]
 
 document.getElementsByClassName("menu-btn")[0].addEventListener("click", () => {
     drawer.classList.add("active")
-
     const isdrawerActive: boolean = drawer.classList.contains("active")
     assignActiveClassToDrawerButton(isdrawerActive)
 })
